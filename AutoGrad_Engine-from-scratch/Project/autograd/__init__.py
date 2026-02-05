801c02c3de47304d87b912d4ee785684caf5054e
Nikesh Patel <nikesh.patel05@gmail.com>
2026-02-05 20:50:58 +0530

    started tinynn project

---
+++ AutoGrad_Engine-from-scratch/Project/autograd/__init__.py
@@ -0,0 +1,22 @@
+from .engine import Node, backward, zero_grad
+from .ops import (
+    add,
+    mul,
+    neg,
+    pow,
+    exp,
+    log,
+    sin,
+)
+__all__ = [
+    "Node",
+    "backward",
+    "zero_grad",
+    "add",
+    "mul",
+    "neg",
+    "pow",
+    "exp",
+    "log",
+    "sin",
+]
